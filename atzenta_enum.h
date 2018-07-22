72dab119b6cb46fc4c9790f19d35617a9c825105
Stavros Filippidis <SFilippidis@users.noreply.github.com>
2018-07-22 23:54:31 +0300

    new email address

--- atzenta_enum.h
+++ atzenta_enum.h
@@ -1,7 +1,7 @@
 // Header file for ATZENTA_* enum for the respective fields.
 
-// Copyright (C) 2011-2017 Stavros Filippidis
-// Contact: stavros@filippidis.name
+// Copyright (C) 2011-2018 Stavros Filippidis
+// Contact: sfilippidis@gmail.com
 
 // This file is part of QGreatstAtzenta.
 
