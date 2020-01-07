ba0569c51d35432643510b736ea6cd1bed50f72a
Stavros Filippidis <SFilippidis@users.noreply.github.com>
2020-01-07 22:41:36 +0200

    Update mainwindow_createorloaddb.cpp

--- mainwindow_createorloaddb.cpp
+++ mainwindow_createorloaddb.cpp
@@ -1,6 +1,6 @@
 // MainWindow::createOrLoadDb method implementation.
 
-// Copyright (C) 2011-2018 Stavros Filippidis
+// Copyright (C) 2011-2020 Stavros Filippidis
 // Contact: sfilippidis@gmail.com
 
 // This file is part of QGreatstAtzenta.
