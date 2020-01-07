f46f68de6f9bb9ed4fba8988f0e51955f43bb09b
Stavros Filippidis <SFilippidis@users.noreply.github.com>
2020-01-07 22:46:26 +0200

    Update mainwindow_usedb.cpp

--- mainwindow_usedb.cpp
+++ mainwindow_usedb.cpp
@@ -1,6 +1,6 @@
 // MainWindow::useDb method implementation.
 
-// Copyright (C) 2011-2018 Stavros Filippidis
+// Copyright (C) 2011-2020 Stavros Filippidis
 // Contact: sfilippidis@gmail.com
 
 // This file is part of QGreatstAtzenta.
