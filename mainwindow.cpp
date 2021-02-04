80024d427da6a7843dcc5c9d5b8cd38b2ea15b18
Stavros Filippidis <SFilippidis@users.noreply.github.com>
2021-02-04 20:41:53 +0200

    New email

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,7 @@
 // Source code for the main window of the application.
 
 // Copyright (C) 2011-2021 Stavros Filippidis
-// Contact: sfilippidis@gmail.com
+// Contact: stavros@filippidis.name
 
 // This file is part of QGreatstAtzenta.
 
